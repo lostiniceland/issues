47fd1b3d5d96a50459dd3c17ed0d02104deea9fc
Marc Schlegel <marc.schlegel@gmx.de>
2015-04-06 20:30:26 +0200

    jsp-module added and little refactoring

---
+++ pax-web/pax-web-itest/src/test/java/issues.pax.web.jsf.test/JspIntegrationTest.java
@@ -0,0 +1,176 @@
+package issues.pax.web.jsf.test;
+
+
+import org.hamcrest.CustomTypeSafeMatcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.ops4j.pax.web.service.spi.WebListener;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.hasItem;
+import static org.ops4j.pax.exam.CoreOptions.*;
+
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class JspIntegrationTest {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Inject
+    protected BundleContext bundleContext;
+
+    //@Inject
+    HttpTestClient httpTestClient;
+    protected WebListener webListener;
+
+    @Inject
+    private BundleContext context;
+
+    @Configuration
+    public Option[] config() {
+        return options(
+                workingDirectory("target/paxexam"),
+                cleanCaches(true),
+
+                // Framework
+                // mavenBundle("org.apache.felix", "org.apache.felix.eventadmin").version("1.4.2"),
+                mavenBundle("org.apache.felix", "org.apache.felix.configadmin").version("1.8.2"),
+                //mavenBundle("org.apache.felix", "org.apache.felix.metatype").version("1.0.10"),
+                //mavenBundle("org.apache.felix", "org.apache.felix.scr").version("1.8.2"),
+                // MyFaces
+                mavenBundle("org.apache.myfaces.core", "myfaces-api").version("2.2.7"),
+                mavenBundle("org.apache.myfaces.core", "myfaces-impl").version("2.2.7"),
+                mavenBundle("javax.annotation", "javax.annotation-api").version("1.2"),
+                mavenBundle("javax.interceptor", "javax.interceptor-api").version("1.2"),
+                mavenBundle("javax.enterprise", "cdi-api").version("1.2"),
+                mavenBundle("javax.validation", "validation-api").version("1.1.0.Final"),
+                mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.javax-inject").version("1_2"),
+                mavenBundle("commons-codec", "commons-codec").version("1.10"),
+                mavenBundle("commons-beanutils", "commons-beanutils").version("1.8.3"),
+                mavenBundle("commons-collections", "commons-collections").version("3.2.1"),
+                mavenBundle("commons-digester", "commons-digester").version("1.8.1"),
+                // Pax-Exam
+                mavenBundle("org.ops4j.base", "ops4j-base-io").version("1.5.0"),
+                mavenBundle("org.ops4j.base", "ops4j-base-lang").version("1.5.0"),
+                mavenBundle("org.ops4j.base", "ops4j-base-monitors").version("1.5.0"),
+                mavenBundle("org.ops4j.base", "ops4j-base-store").version("1.5.0"),
+                mavenBundle("org.ops4j.base", "ops4j-base-util").version("1.5.0"),
+                mavenBundle("org.ops4j.base", "ops4j-base-util-property").version("1.5.0"),
+                mavenBundle("org.ops4j.pax.swissbox", "pax-swissbox-tracker").version("1.8.0"),
+                mavenBundle("org.ops4j.pax.swissbox", "pax-swissbox-lifecycle").version("1.8.0"),
+                mavenBundle("org.ops4j.pax.swissbox", "pax-swissbox-extender").version("1.8.0"),
+                mavenBundle("org.ops4j.pax.swissbox", "pax-swissbox-core").version("1.8.0"),
+                mavenBundle("org.ops4j.pax.exam", "pax-exam").version("4.4.0"),
+                mavenBundle("org.ops4j.pax.exam", "pax-exam-extender-service").version("4.4.0"),
+                mavenBundle("org.ops4j.pax.exam", "pax-exam-inject").version("4.4.0"),
+                mavenBundle().groupId("org.ops4j.pax.logging").artifactId("pax-logging-logback").version("1.8.2"),
+                mavenBundle().groupId("org.ops4j.pax.logging").artifactId("pax-logging-api").version("1.8.2"),
+                // Pax-Web
+                mavenBundle().groupId("org.ops4j.pax.web").artifactId("pax-web-spi").version("4.1.1"),
+                mavenBundle().groupId("org.ops4j.pax.web").artifactId("pax-web-api").version("4.1.1"),
+                mavenBundle().groupId("org.ops4j.pax.web").artifactId("pax-web-extender-war").version("4.1.1"),
+                mavenBundle().groupId("org.ops4j.pax.web").artifactId("pax-web-jsp").version("4.1.1"),
+                mavenBundle().groupId("org.ops4j.pax.web").artifactId("pax-web-jetty-bundle").version("4.1.1"),
+                mavenBundle().groupId("org.eclipse.jdt.core.compiler").artifactId("ecj").version("4.4"),
+                // Others
+                mavenBundle().groupId("org.ops4j.pax.url").artifactId("pax-url-aether").version("2.4.0").type("jar"),
+                mavenBundle().groupId("org.ops4j.pax.url").artifactId("pax-url-war").type("jar").classifier("uber").version("2.4.0"),
+                wrappedBundle(mavenBundle("org.apache.httpcomponents", "httpcore").version("4.3.3")),
+                wrappedBundle(mavenBundle("org.apache.httpcomponents", "httpmime").version("4.3.6")),
+                wrappedBundle(mavenBundle("org.apache.httpcomponents", "httpclient").version("4.3.6")),
+                mavenBundle().groupId("org.apache.xbean").artifactId("xbean-reflect").version("4.1"),
+                mavenBundle().groupId("org.apache.xbean").artifactId("xbean-finder").version("4.1"),
+                mavenBundle().groupId("org.apache.xbean").artifactId("xbean-finder-shaded").version("4.1"),
+                mavenBundle().groupId("org.apache.xbean").artifactId("xbean-bundleutils").version("4.1"),
+                mavenBundle().groupId("org.apache.xbean").artifactId("xbean-asm5-shaded").version("4.1"),
+                mavenBundle().groupId("org.ow2.asm").artifactId("asm-all").version("5.0.3"),
+                // Finally, the WAB (SUT)
+                bundle("reference:file:../pax-web-jsp/target/pax-web-jsp-0.0.1-SNAPSHOT.jar"),
+                junitBundles(), //kein hamcrest-all
+                systemProperty("org.ops4j.pax.url.mvn.localRepository").value("~/.m2/repository"),
+                systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("DEBUG"),
+                systemProperty("org.osgi.service.http.hostname").value("127.0.0.1"),
+                systemProperty("org.osgi.service.http.port").value("8181"),
+                systemProperty("java.protocol.handler.pkgs").value("org.ops4j.pax.url"),
+                systemProperty("org.ops4j.pax.url.war.importPaxLoggingPackages").value("false"),
+                systemProperty("org.ops4j.pax.web.log.ncsa.enabled").value("true"),
+                systemProperty("org.ops4j.pax.web.log.ncsa.directory").value("target/logs"),
+                systemProperty("org.ops4j.pax.web.jsp.scratch.dir").value("target/paxexam/scratch-dir"),
+                systemProperty("ProjectVersion").value("0.0.1-SNAPSHOT"),
+                //frameworkProperty("osgi.console").value("6666"),
+                //frameworkProperty("osgi.console.enable.builtin").value("true"),
+                //frameworkProperty("felix.bootdelegation.implicit").value("true"),
+                systemProperty("org.ops4j.pax.url.mvn.certificateCheck").value("false")
+        );
+    }
+
+    @Before
+    public void setUpITestBase() throws Exception {
+        httpTestClient = new HttpTestClient();
+        initWebListener();
+        //waitForServer("http://127.0.0.1:8181/");
+        waitForWebListener();
+    }
+
+    @After
+    public void tearDownITestBase() throws Exception {
+        httpTestClient.close();
+        httpTestClient = null;
+    }
+
+    @Test
+    public void testInstalledBundle() throws Exception{
+        assertThat(Arrays.asList(context.getBundles()), hasItem(new CustomTypeSafeMatcher<Bundle>("issues.pax-web-jsf Bundle (active)") {
+            @Override
+            protected boolean matchesSafely(Bundle item) {
+                return "issues.pax-web-jsp".equals(item.getSymbolicName()) && item.getState() == Bundle.ACTIVE;
+            }
+        }));
+    }
+
+    protected void initWebListener() {
+        webListener = new WebListenerImpl();
+        bundleContext.registerService(WebListener.class, webListener, null);
+    }
+
+    @Test
+    public void testDispatchJsp() throws Exception {
+        httpTestClient.testWebPath("http://127.0.0.1:8181/osgi-jsp/index.jsp", "It works");
+    }
+
+
+    protected void waitForServer(final String path) throws InterruptedException {
+        new WaitCondition("server") {
+            @Override
+            protected boolean isFulfilled() throws Exception {
+                return httpTestClient.checkServer(path);
+            }
+        }.waitForCondition();
+    }
+
+    protected void waitForWebListener() throws InterruptedException {
+        new WaitCondition("webapp startup") {
+            @Override
+            protected boolean isFulfilled() {
+                return ((WebListenerImpl) webListener).gotEvent();
+            }
+        }.waitForCondition();
+    }
+
+
+}
---
+++ pax-web/pax-web-itest/src/test/java/issues.pax.web.jsf.test/WaitCondition.java
@@ -0,0 +1,41 @@
+package issues.pax.web.jsf.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Marc Klinger - mklinger[at]nightlabs[dot]de
+ */
+public abstract class WaitCondition {
+    private static final long WAIT_TIMEOUT_MILLIS = 10000;
+    private static final Logger LOG = LoggerFactory.getLogger(WaitCondition.class);
+    private String description;
+
+    protected WaitCondition(String description) {
+        this.description = description;
+    }
+
+    protected String getDescription() {
+        return description;
+    }
+
+    protected abstract boolean isFulfilled() throws Exception;
+
+    public void waitForCondition() throws InterruptedException {
+//CHECKSTYLE:OFF
+        long startTime = System.currentTimeMillis();
+        try {
+            while (!isFulfilled() && System.currentTimeMillis() < startTime + WAIT_TIMEOUT_MILLIS) {
+                Thread.sleep(100);
+            }
+            if (!isFulfilled()) {
+                LOG.warn("Waited for {} for {} ms but condition is still not fulfilled", getDescription(), System.currentTimeMillis() - startTime);
+            } else {
+                LOG.info("Successfully waited for {} for {} ms", getDescription(), System.currentTimeMillis() - startTime);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Error waiting for " + getDescription(), e);
+        }
+//CHECKSTYLE:ON
+    }
+}
